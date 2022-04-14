16d43795ccf7de5eafaabaec86c6447b2a3c1f07
henriqueferrazo <henriqueoliver9@gmail.com>
2022-04-14 13:08:22 -0300

    feat: dando inicio a pagina de cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import Header from './Components/Header/Header.jsx' 
-import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './Components/Header/Header.jsx'
+import Register from './Components/Register/Register.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <Router>
       <Header/>
+      <Routes>
+        <Route path='/cadastro' element={<Register/>}/>
+      </Routes>
     </Router>
   );
 }
---
+++ src/Components/Register/Register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Register(){
+    return(
+        <>
+            <h1>Henrique</h1>
+        </>
+    )
+}
+
+export default Register;
